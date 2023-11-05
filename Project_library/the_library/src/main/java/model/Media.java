bb0d320c0b3bb0c52f2de4ad63ddf1f1b945cc94
“sraosha47” <sorusch.afkhami@gmail.com>
2023-11-05 17:54:45 +0100

    Created the Meadia Class

--- Project_library/the_library/src/main/java/model/Media.java
+++ Project_library/the_library/src/main/java/model/Media.java
@@ -1,2 +1,37 @@
-public class media {
+package model;
+
+public abstract class Media {
+    private String title;
+    private Boolean available;
+    private int amount;
+
+    public Media(String title, String available, int amount) {
+        this.title = title;
+        this.amount = amount;
+        this.available = true;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public Boolean getAvailable() {
+        return available;
+    }
+
+    public void setTelefonnummer(Boolean available) {
+        this.available = available;
+    }
 }
