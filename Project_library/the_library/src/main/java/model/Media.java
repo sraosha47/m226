cb54549c015c44549b78f16b4ece095ea6610b1f
“sraosha47” <sorusch.afkhami@gmail.com>
2023-11-05 18:49:03 +0100

    corrected condition for returnMedium method

--- Project_library/the_library/src/main/java/model/Media.java
+++ Project_library/the_library/src/main/java/model/Media.java
@@ -44,7 +44,7 @@ public abstract class Media {
 
     public void returnMedium(int amount) {
         this.amount ++;
-        if (amount == 0){
+        if (amount != 0){
             this.available=true;
         }
     }
