78ef44d375148e4ca9a7b511c0b086754ea2a5d6
“sraosha47” <sorusch.afkhami@gmail.com>
2023-11-05 17:54:09 +0100

    Created the user User Class

--- Project_library/the_library/src/main/java/model/User.java
+++ Project_library/the_library/src/main/java/model/User.java
@@ -1,2 +1,46 @@
-public class user {
+package model;
+
+/**
+ * Backend Class for Person
+ */
+public abstract class User {
+    private String vorname;
+    private String nachname;
+    private String telefonnummer;
+    private String email;
+
+    public User(String vorname, String nachname, String telefonnummer, String email) {
+        this.vorname = vorname;
+        this.nachname = nachname;
+        this.telefonnummer = telefonnummer;
+        this.email = email;
+    }
+
+    public String getVorname() {
+        return vorname;
+    }
+
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
+
+    public String getNachname() {
+        return nachname;
+    }
+
+    public void setNachname(String nachname) {
+        this.nachname = nachname;
+    }
+
+    public String getTelefonnummer() {
+        return telefonnummer;
+    }
+
+    public void setTelefonnummer(String telefonnummer) {
+        this.telefonnummer = telefonnummer;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
