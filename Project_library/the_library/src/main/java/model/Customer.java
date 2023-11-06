0126f00de7b55a1805bc59f6c2e6a15acd41a9fc
Safe6er69 <kurtajleon533@gmail.com>
2023-11-06 20:48:42 +0100

    "changed the string of Customer.java & added email-String"

--- Project_library/the_library/src/main/java/model/Customer.java
+++ Project_library/the_library/src/main/java/model/Customer.java
@@ -4,6 +4,6 @@ package model;
 public abstract class Customer extends User {
 
     public Customer(String firstname, String surname, String phone, String email) {
-        super(vorname, nachname, telefonnummer);
+        super(firstname, surname, phone, email);
     }
 }
