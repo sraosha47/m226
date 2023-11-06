584f50b29ec4962a3ad70a9ff1c3cd7ddfb450f2
Safe6er69 <kurtajleon533@gmail.com>
2023-11-06 20:34:58 +0100

    "Added public User"

--- Project_library/the_library/src/main/java/model/Customer.java
+++ Project_library/the_library/src/main/java/model/Customer.java
@@ -1,2 +1,6 @@
 public abstract class Customer extends User {
+
+    public User(String firstname, String surname, String phone, String email) {
+        super(vorname, nachname, telefonnummer);
+    }
 }
