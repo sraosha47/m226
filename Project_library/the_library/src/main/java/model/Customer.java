9bd3ba9c744f5d0331811131fdc8963eeed88a01
Safe6er69 <kurtajleon533@gmail.com>
2023-11-06 20:53:55 +0100

    "Added email to super"

--- Project_library/the_library/src/main/java/model/Customer.java
+++ Project_library/the_library/src/main/java/model/Customer.java
@@ -4,6 +4,6 @@ package model;
 public abstract class Customer extends User {
 
     public Customer(String firstname, String surname, String phone, String email) {
-        super(firstname, surname, phone, email);
+        super(firstname, surname, phone, emai);
     }
 }
--- Project_library/the_library/src/main/java/model/Librarian.java
+++ Project_library/the_library/src/main/java/model/Librarian.java
@@ -4,6 +4,6 @@ package model;
 public abstract class Librarian extends User {
 
     public Librarian(String firstname, String surname, String phone, String email) {
-        super(firstname, surname, phone, email);
+        super(firstname, surname, phone, emai);
     }
 }
