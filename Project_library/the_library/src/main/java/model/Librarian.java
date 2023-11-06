47624ba35f8dbf04e8e93b5d5e2654369904b204
Safe6er69 <kurtajleon533@gmail.com>
2023-11-06 20:54:31 +0100

    "Fixed syntax emai to email"

--- Project_library/the_library/src/main/java/model/Librarian.java
+++ Project_library/the_library/src/main/java/model/Librarian.java
@@ -4,6 +4,6 @@ package model;
 public abstract class Librarian extends User {
 
     public Librarian(String firstname, String surname, String phone, String email) {
-        super(firstname, surname, phone, emai);
+        super(firstname, surname, phone, email);
     }
 }
