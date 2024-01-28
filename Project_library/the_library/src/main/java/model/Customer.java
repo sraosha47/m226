936537002e5d7190b8d363f73d39857133a8b21b
“sraosha47” <sorusch.afkhami@gmail.com>
2024-01-29 00:33:26 +0100

    add arraylist media, rentmedia adds timestamp and adds medium to list, new method list lists all media, add new returnMedia method

--- Project_library/the_library/src/main/java/model/Customer.java
+++ Project_library/the_library/src/main/java/model/Customer.java
@@ -1,17 +1,38 @@
 package model;
 
+import java.util.ArrayList;
 
 public class Customer extends User {
 
+    public ArrayList<Media> media = new ArrayList<>();
     public Customer(String firstname, String surname, String phone, String email) {
         super(firstname, surname, phone, email);
     }
 
     public void rentMedia(Media medium){
-
         if(medium.getAvailable()){
             medium.switchAvailablity();
             medium.setOwner(this.getFullname());
+            medium.setTimestamp();
+            media.add(medium);
         }
     }
+
+    public void list() {
+        for(Media medium : media) {
+            medium.info();
+            System.out.println("Owned by: " + medium.getOwner() + "\n");
+        }
+    }
+
+    public void returnMedia(Media medium) {
+        if(medium.getOwner() == this.getFullname()){
+            medium.switchAvailablity();
+            medium.setOwner("");
+        } else {
+            System.out.println("This article is not in your possession.");
+        }
+    }
+
+
 }
