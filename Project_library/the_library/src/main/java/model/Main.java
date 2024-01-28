a5611dfd31ded5f700cb5fd1c283cfdd8949b1dc
“sraosha47” <sorusch.afkhami@gmail.com>
2024-01-28 21:22:46 +0100

    add lines to test book and user as well as methods

--- Project_library/the_library/src/main/java/model/Main.java
+++ Project_library/the_library/src/main/java/model/Main.java
@@ -1,12 +1,14 @@
-import model.Book;
-import model.Customer;
+package model;
 
 public class Main {
     public static void main(String[] args) {
         Customer example = new Customer("Ted", "Lasso", "0797776669", "ted.lasso@home.com" );
-        Book newBook = new Book() {
-        }
+        Book newBook = new Book("Knulp", "Hermann Hesse");
+
         System.out.println(example.getFirstname());
         System.out.println(example.getFullname());
+        System.out.println(newBook.getTitle() + " by " + newBook.getAuthor() + "/n It is owned by " + newBook.getOwner());
+        example.rentMedia(newBook);
+        System.out.println(newBook.getTitle() + " by " + newBook.getAuthor() + "/n It is owned by " + newBook.getOwner());
     }
 }
