7b0dcba6ecebb4f5b794d7ac9f5442723c9f3109
“sraosha47” <sorusch.afkhami@gmail.com>
2024-01-28 20:20:57 +0100

    remove abstract modifier

--- Project_library/the_library/src/main/java/model/Librarian.java
+++ Project_library/the_library/src/main/java/model/Librarian.java
@@ -1,7 +1,7 @@
 package model;
 
 
-public abstract class Librarian extends User {
+public class Librarian extends User {
 
     public Librarian(String firstname, String surname, String phone, String email) {
         super(firstname, surname, phone, email);
