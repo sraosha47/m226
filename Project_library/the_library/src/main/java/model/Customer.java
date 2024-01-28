e2e909c0c9cec2237f389f17bd0ccd01ac577151
“sraosha47” <sorusch.afkhami@gmail.com>
2024-01-28 21:21:18 +0100

    add method to rent media

--- Project_library/the_library/src/main/java/model/Customer.java
+++ Project_library/the_library/src/main/java/model/Customer.java
@@ -6,4 +6,12 @@ public class Customer extends User {
     public Customer(String firstname, String surname, String phone, String email) {
         super(firstname, surname, phone, email);
     }
+
+    public void rentMedia(Media medium){
+
+        if(medium.getAvailable()){
+            medium.switchAvailablity();
+            medium.setOwner(this.getFullname());
+        }
+    }
 }
