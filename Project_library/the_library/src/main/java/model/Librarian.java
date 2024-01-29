69dca36dae8f6864f7da6bc3d53367fad5bdb9e6
Safe6er69 <kurtajleon533@gmail.com>
2024-01-29 22:05:13 +0100

    Added Arraylist in User

--- Project_library/the_library/src/main/java/model/Librarian.java
+++ Project_library/the_library/src/main/java/model/Librarian.java
@@ -8,4 +8,5 @@ public class Librarian extends User {
     public Librarian(String firstname, String surname, String phone, String email) {
         super(firstname, surname, phone, email);
     }
+
 }
