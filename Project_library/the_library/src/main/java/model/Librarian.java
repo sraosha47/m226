f87b066852649f4c45d29185a4dcc62decec8540
Safe6er69 <kurtajleon533@gmail.com>
2024-01-29 22:21:43 +0100

    Changed Librarian

--- Project_library/the_library/src/main/java/model/Librarian.java
+++ Project_library/the_library/src/main/java/model/Librarian.java
@@ -8,5 +8,4 @@ public class Librarian extends User {
     public Librarian(String firstname, String surname, String phone, String email) {
         super(firstname, surname, phone, email);
     }
-
 }
