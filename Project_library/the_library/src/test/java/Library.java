98d0e676f070fe2a14307160600b6c49219a0524
“sraosha47” <sorusch.afkhami@gmail.com>
2024-01-29 23:14:06 +0100

    new class to add media to array and to list all available media

--- Project_library/the_library/src/test/java/Library.java
+++ Project_library/the_library/src/test/java/Library.java
@@ -1,2 +1,36 @@
+import model.Media;
+
+import java.util.ArrayList;
+
 public class Library {
+
+    private static Library instance;
+    private ArrayList<Media> media = new ArrayList<>();
+
+    private Library(){}
+
+    public static Library getInstance(){
+        if (instance == null) {
+            synchronized (Library.class) {
+                if (instance == null) {
+                    instance = new Library();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public void addMedium(Media medium) {
+       media.add(medium);
+    }
+
+    public void listMedia(){
+        System.out.println("Following articles are available: ");
+        for (Media medium : media) {
+            if (medium.getAvailable()) {
+                medium.info();
+            }
+        }
+
+    }
 }
