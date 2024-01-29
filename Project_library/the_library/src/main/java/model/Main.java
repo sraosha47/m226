a08c5716ef51c70c55dfb6fa4625c79418fc1bcf
“sraosha47” <sorusch.afkhami@gmail.com>
2024-01-29 20:37:21 +0100

    some code to check if returnMedia works

--- Project_library/the_library/src/main/java/model/Main.java
+++ Project_library/the_library/src/main/java/model/Main.java
@@ -5,9 +5,13 @@ public class Main {
         Customer example = new Customer("Ted", "Lasso", "0797776669", "ted.lasso@home.com" );
         Book newBook = new Book("Knulp", "Hermann Hesse");
         Book newBook2 = new Book("Hyperion", "Friedrich Hölderlin");
+        Cd newCd = new Cd("The Colours and Shapes", "The Foofighters");
 
         example.rentMedia(newBook);
         example.rentMedia(newBook2);
+        example.rentMedia(newCd);
+        example.list();
+        example.returnMedia(newBook);
         example.list();
     }
 }
