54f17889561482b26d7f0fc0dde975acc2afe55e
“sraosha47” <sorusch.afkhami@gmail.com>
2024-01-29 21:42:58 +0100

    add isbn attribute

--- Project_library/the_library/src/main/java/model/Book.java
+++ Project_library/the_library/src/main/java/model/Book.java
@@ -2,9 +2,10 @@ package model;
 
 
 public class Book extends Media {
-
-    public Book(String title, String author) {
+    private String isbn;
+    public Book(String title, String author, String isbn) {
         super(title, author);
+        this.isbn = isbn;
     }
 
 }
