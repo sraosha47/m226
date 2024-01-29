0e9a245384441fc33e22a3a712fce92a31133667
Safe6er69 <kurtajleon533@gmail.com>
2024-01-29 23:28:57 +0100

    LibraryTests erstellt

---
+++ Project_library/the_library/src/test/java/LibraryTests.java
@@ -0,0 +1,46 @@
+import static org.mockito.Mockito.*;
+
+import model.Library;
+import model.Media;
+import org.junit.jupiter.api.Test;
+
+public class LibraryTests {
+
+    @Test
+    public void testAddMedium() {
+        // Create a mock for the Media class
+        Media mockMedium = mock(Media.class);
+
+        // Create an instance of the Library
+        Library library = Library.getInstance();
+
+        // Call the addMedium method on the Library object
+        library.addMedium(mockMedium);
+
+        // Verify that the addMedium method was called with the mockMedium
+        verify(mockMedium, times(1)).info();
+    }
+
+    @Test
+    public void testListMedia() {
+        // Create a mock for the Media class
+        Media mockMedium = mock(Media.class);
+
+        // Configure the behavior of getAvailable method of the Media mock
+        when(mockMedium.getAvailable()).thenReturn(true);
+
+        // Create an instance of the Library
+        Library library = Library.getInstance();
+
+        // Add the mockMedium to the library
+        library.addMedium(mockMedium);
+
+        // Call the listMedia method on the Library object
+        library.listMedia();
+
+        // Verify that the expected methods were called on the Media mock
+        verify(mockMedium, times(1)).info();
+    }
+
+    // Add more test methods as needed
+}
