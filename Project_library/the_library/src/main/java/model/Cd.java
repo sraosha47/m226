a2529aecd4561d4f9ed3023c9864e2ef296ab583
Safe6er69 <kurtajleon533@gmail.com>
2024-01-30 00:30:29 +0100

    Added Attributes for CD

--- Project_library/the_library/src/main/java/model/Cd.java
+++ Project_library/the_library/src/main/java/model/Cd.java
@@ -2,9 +2,10 @@ package model;
 
 
 public class Cd extends Media {
+    private String artist;
 
-    public Cd(String title, String author) {
-        super(title, author);
+    public Cd(String title, String artist) {
+        super(title, artist); // CDs may not have a specific author, passing an empty string
+        this.artist = artist;
     }
-
 }
